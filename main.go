3ccaa86fddbb02b6b03b7431ac804825f309d04a
Akonwi Ngoh <akonwi@gmail.com>
2025-02-28 12:19:37 -0500

    run for select teams

--- main.go
+++ main.go
@@ -6,12 +6,22 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"time"
 )
+
+var teams map[string]string = map[string]string{}
+
+func init() {
+	teams["Manchester United"] = "19538871"
+	teams["Wolverhampton Wanderers"] = "8cec06e1"
+	teams["Ipswich Town"] = "b74092de"
+	teams["Tottenham Hotspurs"] = "361ca564"
+	teams["Aston Villa"] = "8602292d"
+}
 
 const api_key string = "IaF7Bu1ERddp13pUOg0l-vfc95MvbWMFUAohi_yk840"
 const england string = "ENG"
 const premier_league int = 9
-const man_united string = "19538871"
 
 type match struct {
 	MatchId      string `json:"match_id"`
@@ -62,74 +72,81 @@ type snapshot struct {
 
 func main() {
 	client := &http.Client{}
-	req, err := http.NewRequest("GET", "https://fbrapi.com/teams?team_id=19538871", nil)
-	if err != nil {
-		panic("Error creating request: " + err.Error())
-	}
-	req.Header.Add("X-API-Key", api_key)
-	req.Header.Add("Accept", "application/json")
-	res, err := client.Do(req)
-	if err != nil || res.StatusCode != http.StatusOK {
-		panic("Error fetching team: " + err.Error())
-	}
-	defer res.Body.Close()
 
-	body, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		panic("Error reading response body: " + err.Error())
-	}
+	for name, id := range teams {
+		fmt.Printf("Calculating for %s\n", name)
+		req, err := http.NewRequest("GET", "https://fbrapi.com/teams?team_id="+id, nil)
+		if err != nil {
+			panic(fmt.Errorf("Error creating request for %s: %s", name, err))
+		}
+		req.Header.Add("X-API-Key", api_key)
+		req.Header.Add("Accept", "application/json")
+		res, err := client.Do(req)
+		if err != nil || res.StatusCode != http.StatusOK {
+			panic("Error fetching team: " + err.Error())
+		}
+		defer res.Body.Close()
 
-	team := teamResponse{}
-	json.Unmarshal(body, &team)
+		body, err := ioutil.ReadAll(res.Body)
+		if err != nil {
+			panic("Error reading response body: " + err.Error())
+		}
 
-	snap := snapshot{}
-	for _, m := range team.TeamSchedule.Data {
-		if m.LeagueId == premier_league && m.MatchId != "" {
-			snap.NumGames++
-			snap.GoalsAgainst += m.GoalsAgainst
+		team := teamResponse{}
+		json.Unmarshal(body, &team)
 
-			switch m.GoalsAgainst {
-			case 0:
-				snap.CleanSheets.Total++
-				if m.Result == "W" {
-					snap.CleanSheets.Wins++
-				} else {
-					snap.CleanSheets.Draws++
-				}
+		snap := snapshot{}
+		for _, m := range team.TeamSchedule.Data {
+			if m.LeagueId == premier_league && m.MatchId != "" {
+				snap.NumGames++
+				snap.GoalsAgainst += m.GoalsAgainst
 
-			case 1:
-				snap.OneGoalConceded.Total++
-				switch m.Result {
-				case "W":
-					snap.OneGoalConceded.Wins++
-				case "L":
-					snap.OneGoalConceded.Losses++
-				case "D":
-					snap.OneGoalConceded.Draws++
-				}
+				switch m.GoalsAgainst {
+				case 0:
+					snap.CleanSheets.Total++
+					if m.Result == "W" {
+						snap.CleanSheets.Wins++
+					} else {
+						snap.CleanSheets.Draws++
+					}
 
-			default:
-				// todo: what's the goal difference in these matches?
-				snap.TwoPlusGoalsConceded.Total++
-				switch m.Result {
-				case "W":
-					snap.TwoPlusGoalsConceded.Wins++
-				case "L":
-					snap.TwoPlusGoalsConceded.Losses++
-				case "D":
-					snap.TwoPlusGoalsConceded.Draws++
+				case 1:
+					snap.OneGoalConceded.Total++
+					switch m.Result {
+					case "W":
+						snap.OneGoalConceded.Wins++
+					case "L":
+						snap.OneGoalConceded.Losses++
+					case "D":
+						snap.OneGoalConceded.Draws++
+					}
+
+				default:
+					// todo: what's the goal difference in these matches?
+					snap.TwoPlusGoalsConceded.Total++
+					switch m.Result {
+					case "W":
+						snap.TwoPlusGoalsConceded.Wins++
+					case "L":
+						snap.TwoPlusGoalsConceded.Losses++
+					case "D":
+						snap.TwoPlusGoalsConceded.Draws++
+					}
 				}
 			}
 		}
-	}
 
-	snap.AvgGoalsAgainst = float32(snap.GoalsAgainst) / float32(snap.NumGames)
-	snap.CleanSheetRatio = float32(snap.CleanSheets.Total) / float32(snap.NumGames)
-	snap.DirtySheetRatio = 1 - snap.CleanSheetRatio
-	snap.TwoGoalsConcededRatio = float32(snap.TwoPlusGoalsConceded.Total) / float32(snap.NumGames)
-	// DirtySheetRatio * (TwoPlusGoalsConceded.Total / (OneGoalConceded.Total + TwoPlusGoalsConceded.Total))
-	snap.XOverTwo = snap.DirtySheetRatio * (float32(snap.TwoPlusGoalsConceded.Total) / float32(snap.OneGoalConceded.Total+snap.TwoPlusGoalsConceded.Total))
-	saveSnapshot(man_united, "Manchester United", snap)
+		snap.AvgGoalsAgainst = float32(snap.GoalsAgainst) / float32(snap.NumGames)
+		snap.CleanSheetRatio = float32(snap.CleanSheets.Total) / float32(snap.NumGames)
+		snap.DirtySheetRatio = 1 - snap.CleanSheetRatio
+		snap.TwoGoalsConcededRatio = float32(snap.TwoPlusGoalsConceded.Total) / float32(snap.NumGames)
+		// DirtySheetRatio * (TwoPlusGoalsConceded.Total / (OneGoalConceded.Total + TwoPlusGoalsConceded.Total))
+		snap.XOverTwo = snap.DirtySheetRatio * (float32(snap.TwoPlusGoalsConceded.Total) / float32(snap.OneGoalConceded.Total+snap.TwoPlusGoalsConceded.Total))
+		saveSnapshot(id, name, snap)
+
+		fmt.Println("Waiting...")
+		time.Sleep(time.Duration(time.Second) * 7)
+	}
 }
 
 func saveSnapshot(teamId, name string, snap snapshot) {
